04e643b29e4dd197cc6623db5151c1d825cc4658
Efemena <88979259+Festivemena@users.noreply.github.com>
2024-10-17 09:57:38 +0100

    Add files via upload

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,17 +8,16 @@ const Hero = () => {
         <div className="container">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
             <div className="caption small-2 uppercase text-p3">
-              Video Editing
+              Visual Arts Firm
             </div>
             <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
-              Amazingly simple
+              Your vision, X-ecuted!
             </h1>
             <p className="max-w-440 mb-14 body-1 max-md:mb-10">
-              We designed XORA AI Video Editor to be an easy to use, quick to
-              learn, and surprisingly powerful.
+            We design tools that are effortless to use, quick to master, and packed with powerful features—enabling you to create stunning visuals with ease
             </p>
             <LinkScroll to="features" offset={-100} spy smooth>
-              <Button icon="/images/zap.svg">Try it now</Button>
+              <Button icon="/images/zap.svg">Contact us now</Button>
             </LinkScroll>
           </div>
 
